dbb1c46195c723eac98dbb24fcaacecaef75844f
Geekstrange <65814694+Geekstrange@users.noreply.github.com>
2024-05-09 19:07:13 +0800

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-// background.js
-let color = 'rgba(66,66,66,1)';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('插件主色调为: %c rgba(0,125,255,1)', `color: ${color}`);
-});
